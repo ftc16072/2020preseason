d6a0200647373a4645ab1d639b7ad005d4954ac6
Alan Smith <alan@randomsmiths.com>
2020-04-05 14:58:41 -0400

    Added pot to Programming Board

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/ProgrammingBoard.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/ProgrammingBoard.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
 import com.qualcomm.hardware.motors.RevRobotics40HdHexMotor;
+import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
@@ -15,17 +16,21 @@ public class ProgrammingBoard {
     protected Servo servo;
     protected ColorSensor sensorColor;
     protected DistanceSensor sensorDistance;
+    protected AnalogInput pot;
 
     public void init(HardwareMap hwMap){
         motor = hwMap.get(DcMotor.class, "motor1");
         servo = hwMap.get(Servo.class, "servo1");
         sensorColor = hwMap.get(ColorSensor.class, "color_distance");
         sensorDistance = hwMap.get(DistanceSensor.class, "color_distance");
+        pot = hwMap.get(AnalogInput.class, "pot");
+
     }
     List<QQ_Test> getTests() {
         return Arrays.asList(
                 new QQ_TestMotor("REV Motor", 0.3, motor),
                 new QQ_TestServo("servo", 1,0, servo),
+                new QQ_TestAnalogSensor("pot", pot),
                 new QQ_TestColorSensor("color sensor", sensorColor),
                 new QQ_TestDistanceSensor("distance from color", sensorDistance));
     }
