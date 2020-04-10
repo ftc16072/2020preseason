6f0a0937956867643233cf96fb0a105972f668a0
Alan Smith <alan@randomsmiths.com>
2020-04-10 16:36:31 -0400

    Added javadoc for added method
    so that people could see how to add comments that help in using methods that are made.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/PuzzleProgrammingBoard.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/PuzzleProgrammingBoard.java
@@ -1,6 +1,11 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
 public class PuzzleProgrammingBoard extends ProgrammingBoard {
+    /**
+     * This allows you to set the speed of the motor on the programing board.
+     *
+     * @param speed set the speed of the motor on the board
+     */
     public void setMotorSpeed(double speed) {
         motor.setPower(speed);
     }
