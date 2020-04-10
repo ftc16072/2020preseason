3abee76f61a053fcbe8c22edf9e7e4bfdca45a5b
Alan Smith <alan@randomsmiths.com>
2020-04-10 16:34:08 -0400

    Sample Puzzle and solution

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/PuzzleProgrammingBoard.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+public class PuzzleProgrammingBoard extends ProgrammingBoard {
+    public void setMotorSpeed(double speed) {
+        motor.setPower(speed);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/SamplePuzzleSolution.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.mechanisms.PuzzleProgrammingBoard;
+
+/**
+ * Puzzle: Make the speed of the motor changeable by the position of the joystick on the gamepad
+ * When the gamepad A button is pressed, allow full speed.  Otherwise divide the speed in
+ * half.
+ * Puzzle level: Easy
+ */
+public class SamplePuzzleSolution extends OpMode {
+    private PuzzleProgrammingBoard board = new PuzzleProgrammingBoard();
+
+    @Override
+    public void init() {
+        board.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        // get the speed from the stick, but flip since up on joystick is negative
+        double speed = -gamepad1.left_stick_y;
+        // if button not pressed, only go to half speed...
+        if (!gamepad1.a) {
+            speed = speed / 2;
+        }
+        board.setMotorSpeed(speed);
+    }
+}
