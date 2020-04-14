d151e65d75189fa3343441347abb3190e8e7c483
Chirag Sreedhara <chiragsreedhara@gmail.com>
2020-04-13 20:51:07 -0400

    Puzzle Example 3 was Added

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/PuzzleSolution_Ex3.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.mechanisms.PuzzleProgrammingBoard;
+import org.w3c.dom.ranges.Range;
+
+/**
+ * Puzzle: Have servo "follow" the potentiometer knob so they point the same direction.
+ * Puzzle level: Easy
+ */
+@TeleOp()
+public class PuzzleSolution_Ex3 extends OpMode {
+    private PuzzleProgrammingBoard board = new PuzzleProgrammingBoard();
+
+    @Override
+    public void init() {
+        board.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        double angle = board.getPotAngle();
+        board.setServoAngle(Range.clip(angle,0,180));
+    }
+}
