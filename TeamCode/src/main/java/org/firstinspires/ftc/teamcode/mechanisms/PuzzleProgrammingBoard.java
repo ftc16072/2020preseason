272d0523093db8b4f8ea43ac398ebefff0a1a7c4
philip <philip@randomsmiths.com>
2020-04-13 20:51:40 -0400

    Added a challenge solution

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/PuzzleProgrammingBoard.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/PuzzleProgrammingBoard.java
@@ -21,4 +21,12 @@ public class PuzzleProgrammingBoard extends ProgrammingBoard {
         double pos = Range.scale(angle,0,180,0.0,1.0);
         servo.setPosition(pos);
     }
+
+    /**
+     * this returns the angle of the potentiometer
+     * @return angle of teh potentiometer in [0, 270]
+     */
+    public double potAngle(){
+        return Range.scale(pot.getVoltage(), 0, pot.getMaxVoltage(), 0, 270);
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/PuzzleSolution_Exercise3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/PuzzleSolution_Exercise3.java
@@ -2,16 +2,17 @@ package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.mechanisms.PuzzleProgrammingBoard;
 
 /**
- * Puzzle2 make the servo face directions based on the buttons
+ * Puzzle: turn servo to angle of pot
  *
  * Puzzle level: Easy
  */
 @TeleOp()
-public class PuzzleSolution_Exercise2 extends OpMode {
+public class PuzzleSolution_Exercise3 extends OpMode {
     private PuzzleProgrammingBoard board = new PuzzleProgrammingBoard();
 
     @Override
@@ -21,12 +22,8 @@ public class PuzzleSolution_Exercise2 extends OpMode {
 
     @Override
     public void loop() {
-        if(gamepad1.x){
-            board.setServoAngle(0);
-        } else if (gamepad1.y){
-            board.setServoAngle(90);
-        } else if (gamepad1.b){
-            board.setServoAngle(180);
-        }
+        board.setServoAngle(Range.clip(board.potAngle(), 0, 180));
     }
+
 }
+
