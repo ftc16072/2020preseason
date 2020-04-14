028d3afd98492441e1f333dcab4bae49524db089
Eric W <craftedcuber333@gmail.com>
2020-04-13 20:25:45 -0400

    First version of puzzle

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/PuzzleSolution_Ex1.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.mechanisms.PuzzleProgrammingBoard;
+
+/**
+ * Puzzle: Make the speed of the motor changeable by the position of the joystick on the gamepad
+ * When the gamepad A button is pressed, allow full speed.  Otherwise divide the speed in
+ * half.
+ * Puzzle level: Easy
+ */
+@TeleOp()
+public class PuzzleSolution_Ex1 extends OpMode {
+    private PuzzleProgrammingBoard board = new PuzzleProgrammingBoard();
+
+    @Override
+    public void init() {
+        board.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.a) {
+            board.setMotorSpeed(.5);
+        } else {
+            board.setMotorSpeed(0);
+        }
+    }
+}
