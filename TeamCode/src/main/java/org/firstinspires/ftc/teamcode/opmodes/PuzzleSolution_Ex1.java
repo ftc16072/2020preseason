6fa59c824e56be79cd6ede60145e98d4aec341e4
Alan Smith <alan@randomsmiths.com>
2020-04-13 20:25:09 -0400

    First version of exercise

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/PuzzleSolution_Ex1.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.mechanisms.PuzzleProgrammingBoard;
+
+/**
+ * Puzzle: Turn motor when A button pressed.  Stop when not pressed
+ * Puzzle level: Easy
+ */
+@TeleOp()
+public class PuzzleSolution_Ex1 extends OpMode {
+    private PuzzleProgrammingBoard board = new PuzzleProgrammingBoard();
+
+    @Override
+    public void init() {
+        board.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.a) {
+            board.setMotorSpeed(0.5);
+        } else {
+            board.setMotorSpeed(0.0);
+        }
+    }
+}
