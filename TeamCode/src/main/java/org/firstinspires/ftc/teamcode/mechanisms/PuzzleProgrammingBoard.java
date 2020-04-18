891f693abf3d8b4f5b7cf6108a033c4900e770a3
nbgolla <46076731+nbgolla@users.noreply.github.com>
2020-04-18 16:38:39 -0400

    added example two

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/PuzzleProgrammingBoard.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/PuzzleProgrammingBoard.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
+
+import com.qualcomm.robotcore.util.Range;
+
 public class PuzzleProgrammingBoard extends ProgrammingBoard {
     /**
      * This allows you to set the speed of the motor on the programing board.
@@ -8,5 +11,14 @@ public class PuzzleProgrammingBoard extends ProgrammingBoard {
      */
     public void setMotorSpeed(double speed) {
         motor.setPower(speed);
+    }
+
+    /**
+     * set a servo to a given angle
+     * @param angle number in between 0-180
+     */
+    public void setServoAngle(double angle){
+        double pos = Range.scale(angle,0,180,0,1);
+        servo.setPosition(pos);
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/puzzle_ex2.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.mechanisms.PuzzleProgrammingBoard;
+
+/**
+ * set servo  if x is pressed 90 when y is pressed and 180 when b is pressed
+ * Puzzle level: Easy
+ */
+@TeleOp()
+public class puzzle_ex2 extends OpMode {
+    private PuzzleProgrammingBoard board = new PuzzleProgrammingBoard();
+
+    @Override
+    public void init() {
+        board.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        if(gamepad1.x){
+            board.setServoAngle(0);
+        }
+        else if (gamepad1.y){
+            board. setServoAngle(90);
+        }
+        else if (gamepad1.b){
+            board. setServoAngle(180);
+        }
+    }
+}
